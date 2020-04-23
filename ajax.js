a5f51005a2ca649a2ed3de8b6fb36da8f809059e
Rookie <Simon.Lu@grapecity.com>
2020-04-23 12:27:29 +0800

    first version

--- ajax.js
+++ ajax.js
@@ -1,71 +1,60 @@
 export default class Ajax {
-
-  static getPromiseJSON(url) {
-    const promise = new Promise( (resolve, reject) => {
+  static getPromiseJSON(url, HTTPMethodType, headerMap) {
+    const promise = new Promise((resolve, reject) => {
       const handler = function () {
-        console.log(this);
         if (this.readyState !== 4) {
           return;
         }
         if (this.status === 200) {
-          console.log(this.response);
           resolve(this.response);
         } else {
           reject(new Error(this.statusText));
         }
       };
       const client = new XMLHttpRequest();
-      client.open("GET", url);
-      client.setRequestHeader("Accept", "image/webp,image/apng,image/*,*/*;q=0.8");
+      client.open(HTTPMethodType, url);
+      for (let [key, value] of headerMap) {
+        client.setRequestHeader(key, value);
+      }
       client.onreadystatechange = handler;
-      // client.responseType = "text";
       client.send(null);
-
     });
     return promise;
   }
 
-  static getAsyncJSON(url) {
-    const handler = async function () {
+  static async getAsyncJSON(url, HTTPMethodType, headerMap) {
+    return await new Promise((resolve, reject) => {
+      const handler = function () {
+        if (this.readyState !== 4) {
+          return;
+        }
+        if (this.status === 200) {
+          resolve(this.response);
+        } else {
+          reject(new Error(this.statusText));
+        }
+      };
+      const client = new XMLHttpRequest();
+      client.open(HTTPMethodType, url);
+      for (let [key, value] of headerMap) {
+        client.setRequestHeader(key, value);
+      }
+      client.onreadystatechange = handler;
+      client.send(null);
+    });
+  }
 
-      console.log(this);
-      if (this.readyState !== 4) {
-        return;
-      }
-      if (this.status === 200) {
-        console.log(this.response);
-        await Promise.resolve(this.response);
-      } else {
-        await Promise.reject(new Error(this.statusText));
-      }
-    };
-    const client = new XMLHttpRequest();
-    client.open("GET", url);
+  static async makeAsyncRequest(url, HTTPMethodType, headerMap) {
+    let result = null;
     try {
-     // handler = client.onreadystatechange.bind(this);
-      client.onreadystatechange =  handler;
+      result = await Ajax.getAsyncJSON(url, HTTPMethodType, headerMap);
     } catch (error) {
       console.error('出错了', error);
     }
-    client.responseType = "text";
-    client.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-    client.send();
-    return client.onreadystatechange;
+    console.log(result);
   }
-
-  static makeAsyncRequest() {
-    console.log(Ajax.getAsyncJSON("./package-lock.json"));
-    // Ajax.getAsyncJSON("./package-lock.json").then(function (json) {
-    //   console.log(json);
-    //   for (let index in json) {
-    //     console.log('Contents: ' + index);
-    //   }
-    // }, function (error) {
-    //   console.error('出错了', error);
-    // });
-  }
-  static makePromiseRequest() {
-    Ajax.getPromiseJSON("http://t.weather.sojson.com/api/weather/city/101030100").then(function (result) {
+  static makePromiseRequest(url, HTTPMethodType, headerMap) {
+    Ajax.getPromiseJSON(url, HTTPMethodType, headerMap).then(function (result) {
       console.log(result);
     }, function (error) {
       console.error('出错了', error);
--- main.js
+++ main.js
@@ -2,7 +2,14 @@ import Ajax from './ajax.js';
 
 class Main {
     static start() {
-        document.getElementById('ajaxButton').addEventListener('click', Ajax.makePromiseRequest);
+        document.getElementById('ajaxButton').addEventListener('click', Main.clickHandler);
+    }
+    static clickHandler() {
+        const url = './package.json';
+        const HTTPMethodType = 'GET';
+        let headerMap = new Map();
+        headerMap.set('Accept', 'application/json');
+        Ajax.makePromiseRequest(url, HTTPMethodType, headerMap);
     }
 }
 
